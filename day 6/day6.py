2d74340f66cd0bb50a4a27431389eb7ca299fc25
Jens Bontinck <4587338+jBontinck@users.noreply.github.com>
2023-12-06 21:11:54 +0100

    quickie

--- day 6/day6.py
+++ day 6/day6.py
@@ -1,9 +1,25 @@
+def get_count_of_record_breaking_attempts(game_duration,record):
+    return len(list(filter(lambda x: x > record, list(map(lambda hold: (hold*1)*(game_duration-hold), range(0, game_duration))))))
+
 
 def solution(inputstring):
-    return 0,0
+    time = list(map(lambda x: x.strip(),inputstring.splitlines()[0].split()))
+    duration = list(map(lambda x:x.strip(), inputstring.splitlines()[1].split()))
+
+    print(time)
+    total_part1 = 1
+    for i in range(1,len(time)):
+        total_part1 *= get_count_of_record_breaking_attempts(int(time[i]),int(duration[i]))
+
+    time = ''.join(time[1:]) 
+    duration = ''.join(duration[1:])
+    total_part2 = get_count_of_record_breaking_attempts(int(time),int(duration))
+
+
+    return total_part1,total_part2
 
 if __name__ == "__main__":
-    with open("day 5/input.txt", "r") as f:
+    with open("day 6/input.txt", "r") as f:
         part1, part2 = solution(f.read())
         print(f"Part 1: {part1}")
         print(f"Part 2: {part2}")
